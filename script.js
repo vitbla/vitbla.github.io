296f139afa9b28c5ff8c33541ca0e124df9b8ff4
Arl1n <simetra08@mail.ru>
2026-05-19 22:36:25 +0300

    Fix class to id in script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function init()
 {
     console.log("hi m1curoshi");
-    document.getElementById("head").onclick = () => {
+    document.getElementById("me").onclick = () => {
         alert("Hey");
     }
 }
